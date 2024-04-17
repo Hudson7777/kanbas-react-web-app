7ab40ed9e326962a42c8520fb6ac5ca4b965d5a8
Hudson7777 <1155124383@link.cuhk.edu.hk>
2024-04-17 19:34:14 -0400

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,7 +2,6 @@ import store from "./store";
 import { Provider } from "react-redux";
 
 import Nav from "../Nav";
-import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import Assignment5 from "./a5";
 import { Routes, Route, Navigate } from "react-router";
@@ -12,8 +11,7 @@ function Labs() {
       <div>
         <Nav />
         <Routes>
-          <Route path="/" element={<Navigate to="a3" />} />
-          <Route path="a3" element={<Assignment3 />} />
+          <Route path="/" element={<Navigate to="a4" />} />
           <Route path="a4" element={<Assignment4 />} />
           <Route path="a5" element={<Assignment5 />} />
         </Routes>
